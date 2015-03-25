ec7f2e8af128414b84b205989ead2daa52e09934
Narendra Singh <narendra.singh@synergytechservices.com>
2015-03-25 19:14:42 +0530

    Initial commit for chaplin config, package.json

---
+++ frontend/config.js
@@ -0,0 +1,7 @@
+config.app = 'replace_me';
+config.devFile = config.app + '.js';
+config.appFile = config.app + '.js';
+config.devPath = '../js/dev';
+config.appDir = '/apps';
+config.appPath = config.appDir + '/' + config.appFile;
+
